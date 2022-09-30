f899d855e7382d2c8c6e09b019c28c526d04a877
Hailiang XIE <hadley.tse@gmail.com>
2022-09-30 12:13:41 +0800

    add findByEmail in CustomerRepository and unit tests

--- src/main/java/xie/hailiang/resourceserver/repository/CustomerRepository.java
+++ src/main/java/xie/hailiang/resourceserver/repository/CustomerRepository.java
@@ -1,9 +1,12 @@
 package xie.hailiang.resourceserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import xie.hailiang.resourceserver.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>  {
-
+	
+	@Query("SELECT c FROM Customer c WHERE c.email = ?1")
+	Customer findByEmail(String email);
 }
